b56ab9bec4014e98be7c568003c284aaac1ae43d
Brian DeRocher <brian@derocher.org>
2022-02-01 12:14:05 -0500

    feat: Add noclist module.

---
+++ src/noclist/noclist.py
@@ -0,0 +1,11 @@
+"""The Foo module provides things and functions."""
+
+
+# pylint: disable=too-few-public-methods
+class Noclist:
+    """The Foo class provides sample methods."""
+
+    @staticmethod
+    def do_something() -> bool:
+        """Return true, always."""
+        return True
---
+++ tests/noclist/test_noclist.py
@@ -0,0 +1,7 @@
+"""Test the noclist module."""
+
+from src.noclist import noclist
+
+
+def test_do_something() -> None:
+    assert noclist.Noclist.do_something() is True
