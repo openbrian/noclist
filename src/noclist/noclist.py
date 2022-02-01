b453a6dd049257ae0278cd98c32abe955fe763b9
Brian DeRocher <brian@derocher.org>
2022-02-01 16:26:14 -0500

    feat: Add tests for authenticating using mock responses.
    Include tests for HTTPError and URLError.  I can't get openurl to trip the HTTPError.

--- src/noclist/noclist.py
+++ src/noclist/noclist.py
@@ -1,16 +1,54 @@
 """The Foo module provides things and functions."""
 
 from hashlib import sha256
+from logging import DEBUG, Formatter, Logger, StreamHandler, getLogger
+from sys import stdout
+from typing import Optional
+from urllib.error import URLError
+from urllib.request import Request, urlopen
+
+NOCHOST: str = "http://localhost:8888"
 
 
 class Noclist:
     """The Noclist class calls the adhoc/noclist server."""
 
     @staticmethod
-    def authenticate() -> bool:
-        return True
+    def authenticate() -> Optional[str]:
+        """Returns the token."""
+        url: str = f"{NOCHOST}/auth"
+        request = Request(url)
+        try:
+            # urlopen will handle redirects
+            with urlopen(request) as response:
+                # This lookup is case-insensitive.  If this header is not
+                # present None is returned.
+                return str(response.headers["badsec-authentication-token"])
+        # BCD: Even though I mock out an HTTPError side_effect, urlopen will
+        # raise a URLError!
+        # except HTTPError as error:
+        #     logger = get_logger()
+        #     logger.debug('auth 1 httperror')
+        #     logger.debug(error)
+        #     return None
+        except URLError as error:
+            logger: Logger = get_logger()
+            logger.debug("auth 2 urlerror")
+            logger.debug(error)
+            return None
 
     @staticmethod
     def build_checksum(auth_token: str, request_path: str) -> str:
         target: bytes = str.encode(f"{auth_token}{request_path}")
         return sha256(target).hexdigest()
+
+
+def get_logger() -> Logger:
+    logger = getLogger()
+    logger.setLevel(DEBUG)
+    handler = StreamHandler(stdout)
+    handler.setLevel(DEBUG)
+    formatter = Formatter("DEBUG: %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
--- tests/noclist/test_noclist.py
+++ tests/noclist/test_noclist.py
@@ -1,17 +1,53 @@
-"""Test the noclist module."""
+"""Test the noclist module.  With pytest-mock there's no need for decorator or
+context managers."""
+
+from email.message import Message
+from urllib.error import HTTPError, URLError
+
+from pytest_mock import MockerFixture
 
 from src.noclist import noclist
 
 
-def test_authenticate() -> None:
-    assert noclist.Noclist.authenticate() is True
+def test_authenticate(mocker: MockerFixture) -> None:
+    # Arrange
+    mock_urlopen = mocker.patch("src.noclist.noclist.urlopen")
+    token: str = "5D51D045-EEFE-A60D-090C-CAF9935400FE"
+    context_manager = mocker.MagicMock()
+    # context_manager.getcode.return_value = 200
+    # context_manager.read.return_value = 'http body is irrelevant'
+    context_manager.headers.__getitem__.return_value = token  # any header key
+    context_manager.__enter__.return_value = context_manager
+    mock_urlopen.return_value = context_manager
+    # Act and Assert
+    assert noclist.Noclist.authenticate() == token
+
+
+def test_authenticate_server_down(mocker: MockerFixture) -> None:
+    # Arrange
+    message = "The connection refused"
+    mocker.patch("urllib.request.urlopen", side_effect=URLError(message))
+    # Act and Assert
+    assert noclist.Noclist.authenticate() is None
+
+
+def test_authenticate_http_error(mocker: MockerFixture) -> None:
+    # Arrange
+    message: Message = Message()
+    http_error: HTTPError = HTTPError("url", 42, "msg", message, None)
+    mocker.patch("urllib.request.urlopen", side_effect=http_error)
+    # Act and Assert
+    assert noclist.Noclist.authenticate() is None
 
 
 def test_build_checksum() -> None:
+    # Arrange
     auth_token: str = "12345"
     request_path: str = "/users"
     expected_checksum = (
         "c20acb14a3d3339b9e92daebb173e41379f9f2fad4aa6a6326a696bd90c67419"
     )
+    # Act
     hash_: str = noclist.Noclist.build_checksum(auth_token, request_path)
+    # Assert
     assert hash_ == expected_checksum
